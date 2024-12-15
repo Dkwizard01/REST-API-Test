93a80c8e44df82b2960866114bae7348e49d5acb
Dkwizard01 <104919155+Dkwizard01@users.noreply.github.com>
2024-12-15 15:17:25 +0100

    Added project files.

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class TasksController : ControllerBase
+    {
+        string[] tasks = ["External ID", "Use backend API Microservice", "CosmosDB", "Call direct method", "Return response"];
+        
+       
+        [HttpGet]
+        public string ApiTasks()
+        {
+            return String.Join(",", tasks);
+            
+        }
+        [HttpDelete("{numElements}")]
+        public string DeleteTasks(int numElements)
+        {
+            return String.Join(",", tasks, numElements, tasks.Length - numElements);
+        }
+        [HttpPut("{length}, {position}, {add}")] 
+        public string Addtasks(int length, int position, string add)
+        {
+            string[] nTasks = new string[length];
+            Array.Copy(tasks, nTasks, tasks.Length);
+            nTasks[position] = add;
+            return String.Join(",", nTasks);
+        }
+        [HttpPost("{length}, {position}, {add}")]
+
+        public string NewArray(int length, int position = 0, string add = " ")
+        {
+            string[] nArray = new string[length];
+            nArray[position] = add;
+            return String.Join(",", nArray);
+        }
+    }
+}
